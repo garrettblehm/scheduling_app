cdf7ef949b90082e1a2cb9e2b9781471d816cddd
Garrett Blehm <garrett.blehm@cerner.com>
2021-03-15 11:17:33 -0500

    Init from jumpstart
    Signed-off-by: Garrett Blehm <garrett.blehm@cerner.com>

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ScheduleApp
   class Application < Rails::Application
+    config.application_name = "Scheduling Helper"
+
     config.active_job.queue_adapter = :sidekiq
-    config.application_name = Rails.application.class.module_parent_name
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,20 @@ authenticate :user, lambda { |u| u.admin? } do
   mount Sidekiq::Web => '/sidekiq'
 
   namespace :madmin do
+    namespace :active_storage do
+      resources :variant_records
+    end
+    namespace :active_storage do
+      resources :attachments
+    end
+    resources :announcements
+    resources :notifications
+    namespace :active_storage do
+      resources :blobs
+    end
+    resources :services
+    resources :users
+    root to: "dashboard#show"
   end
 end
 
